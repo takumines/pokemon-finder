9946ce89746d0e5b8900e63a2d979501388b3081
takumines <takumines1@gmail.com>
2024-03-23 13:21:48 +0900

    [update] Layoutコンポーネントの更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import ThemeProvider from "@/app/components/theme-provider";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +18,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <main className="flex min-h-screen flex-col items-center p-24">
+            <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+              <Link href="/">
+                <h2 className="text-2xl text-bold">Pokemon Finder</h2>
+              </Link>
+            </div>
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
