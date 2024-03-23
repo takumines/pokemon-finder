61f78d23991b71cc2c3d1741d68cb5982d192018
takumines <takumines1@gmail.com>
2024-03-23 13:20:52 +0900

    [add] ポケモン関連のコンポーネント作成

---
+++ app/components/pokemon-grid.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import { Label } from "@/app/components/ui/label";
+import { Input } from "@/app/components/ui/input";
+import PokemonCard from "@/app/components/pokemon-card";
+import { NamedAPIResource } from "pokenode-ts";
+
+const PokemonGrid = ({ pokemonList }: { pokemonList: NamedAPIResource[] }) => {
+  const [searchText, setSearchText] = useState("");
+
+  const filteredPokemonList = pokemonList.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchText.toLowerCase()),
+  );
+
+  return (
+    <>
+      <div>
+        <h3 className="text-2xl py-6 text-center">Search For Your Pokemon!</h3>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="pokemonName">Pokemon Name</Label>
+          <Input
+            placeholder="Mew"
+            autoComplete="off"
+            onChange={(e) => setSearchText(e.target.value)}
+            type="text"
+            value={searchText}
+            id="pokemonName"
+          />
+        </div>
+        <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
+      </div>
+      <div className="mb-32 grid grid-cols-3 text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        {filteredPokemonList.map((pokemon) => {
+          return <PokemonCard name={pokemon.name} key={pokemon.name} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default PokemonGrid;
