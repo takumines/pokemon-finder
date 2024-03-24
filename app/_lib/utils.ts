317e145e8f0e0f44c488974c5a6339b5500e974c
takumines <takumines1@gmail.com>
2024-03-24 14:10:38 +0900

    [update] URLからidを取得する関数をついか

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const extractIdFromUrl = (url: string): number | undefined => {
+  const pattern: RegExp = /(\d+)\/?$/;
+  const match = url.match(pattern);
+
+  return match && match[1] ? parseInt(match[1], 10) : undefined;
+};
