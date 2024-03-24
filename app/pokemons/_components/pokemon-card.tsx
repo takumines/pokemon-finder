d507bb51dfa96a6181867fddcac0ca79ef3b8ca1
takumines <takumines1@gmail.com>
2024-03-24 17:58:11 +0900

    [update] 遷移先のURLの変更

--- app/pokemons/_components/pokemon-card.tsx
+++ app/pokemons/_components/pokemon-card.tsx
@@ -24,7 +24,7 @@ const PokemonCardImage = async ({ id, name }: PokemonCardProps) => {
 const PokemonCard = ({ id, name }: PokemonCardProps) => {
   return (
     <Link
-      href={`/pokemon/${id}`}
+      href={`/pokemons/${id}`}
       className="group rounded-lg border border-transparent m-3 px-5 py-4 transition-colors dark:border-gray-500 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
       key={name + "Card"}
     >
