1f4d543710fc2ac14624b502efb07424c8931dca
takumines <takumines1@gmail.com>
2024-03-24 14:15:11 +0900

    [add] ポケモンが見つからないときのNot Found画面を作成

---
+++ app/pokemon/[pokemon-id]/not-found.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const PokemonDetailNotFound = () => {
+  return (
+    <div className="mt-8">
+      <Image
+        src="/pokemon-not-found.png"
+        alt="Pokemon not found"
+        width={200}
+        height={200}
+        className="mx-auto"
+      />
+      <h1 className="text-[32px] mt-8 text-center">Pokemon not found</h1>
+    </div>
+  );
+};
+
+export default PokemonDetailNotFound;
