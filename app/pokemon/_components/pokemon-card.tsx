28452aa1862f0c4db9a54d9a0a859df104732b55
takumines <takumines1@gmail.com>
2024-03-24 17:10:19 +0900

    [update] ポケモン画像表示をsuspenseを使って行うよう修正

--- app/pokemon/_components/pokemon-card.tsx
+++ app/pokemon/_components/pokemon-card.tsx
@@ -1,7 +1,7 @@
+"use client";
 import Link from "next/link";
 import { getPokemonById } from "@/app/_api/pokemon/get-pokemon";
-import { useEffect, useState } from "react";
-import { Pokemon } from "pokenode-ts";
+import { Suspense } from "react";
 import PokemonImage from "@/app/pokemon/_components/pokemon-image";
 import Spinner from "@/app/components/element/spinner";
 
@@ -10,39 +10,27 @@ interface PokemonCardProps {
   name: string;
 }
 
+const PokemonCardImage = async ({ id, name }: PokemonCardProps) => {
+  const pokemon = await getPokemonById(id);
+  const pokemonImage = pokemon?.sprites.other
+    ? pokemon.sprites.other["official-artwork"].front_default
+    : null;
+
+  const image = pokemonImage ? pokemonImage : "pokemon-not-found.png";
+
+  return <PokemonImage image={image} name={name} />;
+};
+
 const PokemonCard = ({ id, name }: PokemonCardProps) => {
-  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-  const fetchPokemon = async (id: number) => {
-    const pokemon = await getPokemonById(id);
-    if (!pokemon) {
-      return;
-    }
-    setPokemon(pokemon);
-  };
-
-  useEffect(() => {
-    fetchPokemon(id);
-  }, [id]);
-
-  const getPokemonImage = (pokemon: Pokemon) => {
-    const image = pokemon.sprites.other
-      ? pokemon.sprites.other["official-artwork"].front_default
-      : null;
-
-    return image ? image : "pokemon-not-found.png";
-  };
-
   return (
     <Link
       href={`/pokemon/${id}`}
       className="group rounded-lg border border-transparent m-3 px-5 py-4 transition-colors dark:border-gray-500 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
       key={name + "Card"}
     >
-      {pokemon ? (
-        <PokemonImage image={getPokemonImage(pokemon)} name={name} />
-      ) : (
-        <Spinner className="mx-auto" />
-      )}
+      <Suspense fallback={<Spinner className="mx-auto" />}>
+        <PokemonCardImage id={id} name={name} />
+      </Suspense>
       <h2 className="text-2xl font-semibold text-center">
         {name.charAt(0).toUpperCase() + name.slice(1)}
       </h2>
