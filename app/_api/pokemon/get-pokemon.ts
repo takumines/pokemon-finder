5c23586c15ef4f00f2f068a3c9dec10c60f09a39
takumines <takumines1@gmail.com>
2024-03-24 14:13:20 +0900

    [update] pokeapiを叩く処理をリファクタ
    fetch関数を利用するようにした

--- app/_api/pokemon/get-pokemon.ts
+++ app/_api/pokemon/get-pokemon.ts
@@ -1,11 +1,29 @@
 import { pokemonApi } from "@/app/_lib/pokemon-api";
+import { NamedAPIResourceList, Pokemon } from "pokenode-ts";
+import { ResponseError } from "@/app/_api/error/response-error";
+import { handlerError } from "@/app/_api/error/handler-error";
 
 export const getPokemonList = async () => {
-  const res = await pokemonApi.listPokemons(0, 151);
+  const res = await pokemonApi(
+    "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0",
+  );
+  const data: NamedAPIResourceList = await res.json();
 
-  return res.results;
+  return data.results;
 };
 
-export const getPokemonByName = async (name: string) => {
-  return await pokemonApi.getPokemonByName(name);
+export const getPokemonById = async (id: number) => {
+  try {
+    const res = await pokemonApi(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+    const data: Pokemon = await res.json();
+
+    return data;
+  } catch (err) {
+    if (err instanceof ResponseError) {
+      if (err.response.status === 404) {
+        return null;
+      }
+    }
+    handlerError(err);
+  }
 };
--- app/_lib/pokemon-api/index.ts
+++ app/_lib/pokemon-api/index.ts
@@ -1,3 +1,10 @@
-import { PokemonClient } from "pokenode-ts";
+import { ResponseError } from "@/app/_api/error/response-error";
 
-export const pokemonApi = new PokemonClient();
+export const pokemonApi = async (url: RequestInfo): Promise<Response> => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new ResponseError("Bad Response", res);
+  }
+
+  return res;
+};
